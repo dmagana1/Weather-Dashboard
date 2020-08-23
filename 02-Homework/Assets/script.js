5d7153284f305200d0afebfe4dfeaa559dee89b4
Diane Magana <dianemagana@outlook.com>
2020-08-22 19:05:50 -0700

    Homework

---
+++ 02-Homework/Assets/script.js
@@ -0,0 +1,12 @@
+function initPage() {
+    const inputEl = document.getElementById("city-input");
+    const searchEl = document.getElementById("searchBtn");
+    const nameEl = document.getElementById("cityName");
+    const currentimgEl = document.getElementById("weatherIcon");
+    const tempEl = document.getElementById("temp");
+    const humidityEl = document.getElementById("humidity");
+    const windEl = document.getElementById("windSpeed");
+    const uvEl = document.getElementById("searchBtn");
+    const searchEl = document.getElementById("uv-Index");
+    
+}
